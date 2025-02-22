47b3acbea60149ac793db52129b8e63d29eaf8b9
Millie Houston <Immaloser102@gmail.com>
2025-02-21 17:47:13 -0800

    minor tweaks to the header

--- project_10_WTWR/src/components/Header.jsx
+++ project_10_WTWR/src/components/Header.jsx
@@ -1,14 +1,19 @@
 import "../blocks/header.css";
 
+import logo from "../images/Logo.svg";
+import avatar from "../images/Ellipse 18.png";
+
 function Header() {
     return (
         <header className="header">
-            <img className="header__logo"/>
-            <p className="header__date-and-location">Date,Location</p>
+            <div className="header__important">
+                <img className="header__logo" src={logo}/>
+                <p className="header__date-and-location">Date,Location</p>
+            </div>
             <button className="header__clothes-btn">Add Clothing</button>
             <div className="header__user-container">
                 <p className="header__username">Name</p>
-                <img src="" alt="" className="header__avatar" />
+                <img src={avatar} alt="UserName" className="header__avatar" />
             </div>
         </header>
     )
