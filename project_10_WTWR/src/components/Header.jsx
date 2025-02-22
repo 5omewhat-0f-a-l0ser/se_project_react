282331b1d711bbe8f72b1c0712457e0ce2a78dbc
Millie Houston <Immaloser102@gmail.com>
2025-02-21 17:32:15 -0800

    rearranged the header horizontally

--- project_10_WTWR/src/components/Header.jsx
+++ project_10_WTWR/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import "../blocks/header.css";
 
 function Header() {
     return (
@@ -5,6 +6,10 @@ function Header() {
             <img className="header__logo"/>
             <p className="header__date-and-location">Date,Location</p>
             <button className="header__clothes-btn">Add Clothing</button>
+            <div className="header__user-container">
+                <p className="header__username">Name</p>
+                <img src="" alt="" className="header__avatar" />
+            </div>
         </header>
     )
 }
