f6413427f8d073b4646b643d18b3c29bb8fd28dd
Millie Houston <immaloser102@gmail.com>
2025-02-25 13:34:23 -0800

    calling it a day for now, got most of the modal styled and structured, will continue this when I return home to charge my laptop

--- project_10_WTWR/src/components/ModalForm.jsx
+++ project_10_WTWR/src/components/ModalForm.jsx
@@ -3,26 +3,39 @@ import "../blocks/modal.css"
 function ModalWithForm() {
     return(
         <div className="modal">
-            <form className="modal__form"></form>
-            <h2 className="modal__title">New Garment</h2>
-            <button type="button" className="modal__close"></button>
-            <label htmlFor="name" className="modal__label">
-                Name {" "}
-                 <input 
-                 type="text" 
-                 className="modal__input" 
-                 id="name" placeholder="name" />
-            </label>
-            <label htmlFor="imageUrl" className="modal__label">
-                Image {" "}
-                 <input 
-                 type="url" 
-                 className="modal__input" 
-                 id="imageUrl" placeholder="Image Url" />
-            </label>
-            <fieldset className="modal__radio-btns">
-                <legend className="modal__legend">Select the weather type:</legend>
-                <label htmlFor="" className="modal__label modal__input_type_radio"></label>            </fieldset>
+            <div className="modal__container">
+                <form className="modal__form">
+                    <h2 className="modal__title">New Garment</h2>
+                    <button type="button" className="modal__close"></button>
+                    <label htmlFor="name" className="modal__label">
+                        Name {" "}
+                         <input 
+                         type="text" 
+                         className="modal__input" 
+                         id="name" placeholder="name" />
+                    </label>
+                    <label htmlFor="imageUrl" className="modal__label">
+                        Image {" "}
+                         <input 
+                         type="url" 
+                         className="modal__input" 
+                         id="imageUrl" placeholder="Image Url" />
+                    </label>
+                    <fieldset className="modal__radio-btns">
+                        <legend className="modal__legend">Select the weather type:</legend>
+                        <label htmlFor="hot" className="modal__label modal__label_type_radio">
+                            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+                        </label>
+                        <label htmlFor="warm" className="modal__label modal__label_type_radio">
+                            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+                        </label>
+                        <label htmlFor="cold" className="modal__label modal__label_type_radio">
+                            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+                        </label>
+                    </fieldset>
+                    <button className="modal__submit">Add Garment</button>
+                </form>
+            </div>
         </div>
     )
 }
