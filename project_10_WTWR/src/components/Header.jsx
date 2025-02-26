800efe6187575958c7bd67f326e7c5ad64527f19
Millie Houston <immaloser102@gmail.com>
2025-02-26 13:02:36 -0800

    Modal's functionality does not work, but the code is there

--- project_10_WTWR/src/components/Header.jsx
+++ project_10_WTWR/src/components/Header.jsx
@@ -6,14 +6,20 @@ import DateDisplay from "../components/DateDisplay";
 import logo from "../images/Logo.svg";
 import avatar from "../images/avatar.png";
 
-function Header() {
+function Header({ onAddBtnClick }) {
      const location = "New York"
 
     return (
         <header className="header">
                 <img className="header__logo" src={logo}/>
                 <DateDisplay className="header__date-and-location"/>
-            <button className="header__clothes-btn">+ Add Clothing</button>
+            <button 
+                onCLick={onAddBtnClick} 
+                type="button" 
+                className="header__clothes-btn"
+            >
+                + Add Clothing
+            </button>
             <div className="header__user-container">
                 <p className="header__username">Terrence Tegegne</p>
                 <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
