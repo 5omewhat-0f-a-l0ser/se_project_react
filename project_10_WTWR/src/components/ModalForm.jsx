39d998dfa7f2532f9f5b982d7cf644e03be84509
Millie Houston <immaloser102@gmail.com>
2025-03-03 13:24:37 -0800

    Trying to get my add clothing button to work now

--- project_10_WTWR/src/components/ModalForm.jsx
+++ project_10_WTWR/src/components/ModalForm.jsx
@@ -1,12 +1,21 @@
+import React, { useState } from 'react';
+
 import "../blocks/modal.css"
 
 function ModalWithForm({ children, buttonText, title, activeModal, closeModal, escCLose, submit}) {
+    const [imageSrc, setImageSrc] = useState("");
+
+    const handleSubmit = (evt) => {
+        event.preventDefault();
+        console.log("image added:", imageSrc);
+    }
+
     return(
         <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
             <div className="modal__container">
                 <h2 className="modal__title">{title}</h2>
                 <button onClick={ closeModal } type="button" className="modal__close"></button>
-                <form className="modal__form">
+                <form onSubmit={handleSubmit} className="modal__form">
                     {children}
                     <button onClick={ submit } className="modal__submit">{buttonText}</button>
                 </form>
