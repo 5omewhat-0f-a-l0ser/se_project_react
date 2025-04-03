8fc8b9286bc2395a0e360c6b82e9ca05db572567
Millie Houston <immaloser102@gmail.com>
2025-04-03 12:36:52 -0700

    Beginning sidebar code

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import avatar from "../images/avatar.png";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <img src="" alt="" className="sidebar__img" />
+        </div>
+    );
+}
+
+export default Sidebar;
