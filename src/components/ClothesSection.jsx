b1a99cee25988e6bfd0916b44a6516276e74c47f
Millie Houston <immaloser102@gmail.com>
2025-04-05 21:32:54 -0700

    Looks like the figma, my arms asleep

--- src/components/ClothesSection.jsx
+++ src/components/ClothesSection.jsx
@@ -8,8 +8,8 @@ function ClothesSection() {
     return (
        <div className="clothes-selection">
             <div className="clothes-selection__description">
-                <p className="clothes-selection__label">YOUR ITEMS</p>
-                <button className="clothes-selection__btn">+ ADD NEW</button>
+                <p className="clothes-selection__label">Your Items</p>
+                <button className="clothes-selection__btn">+ Add New</button>
             </div>
             <ul className="cards__list">
                 {defaultClothingItems
