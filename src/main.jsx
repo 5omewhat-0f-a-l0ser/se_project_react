3256c8cf7602665e6c4b70d24df27e54bde07605
Millie Houston <Immaloser102@gmail.com>
2025-04-16 18:51:47 -0700

    Appaaaaaarently, HashRouter is a thing that breaks your site, good to know

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <HashRouter basename="/se_project_react">
+     <BrowserRouter basename="/se_project_react">
      <App />
-   </HashRouter>
+   </BrowserRouter>
   </React.StrictMode>,
 )
