54978e6dab785eb666aff3e5d8b2158a020f9920
Millie Houston <immaloser102@gmail.com>
2025-05-21 10:33:55 -0700

    finally fixed the problems, if this gets returned one more time, I'm going back to hospital and never fricking leaving that place

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const baseUrl = "http://localhost:3001";
 
 const handleResponse = (res) => {
-  return res.ok ? res.json() : Promise.reject("Error: ${res.status}");
+  return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
 };
 
 function getItems() {
@@ -27,14 +27,14 @@ function addItems({ name = "", imageUrl = "", weather = "" }) {
   }).then(handleResponse);
 }
 
-function deleteItems({ name = "", imageUrl = "", weather = "" }) {
-  return fetch(`${baseUrl}/items`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name, imageUrl, weather }),
-  }).then(handleResponse);
-}
+   function deleteItems(id) {
+     return fetch(`${baseUrl}/items/${id}`, {
+       method: "DELETE",
+       headers: {
+         "Content-Type": "application/json",
+       },
+     }).then(handleResponse);
+   }
+   
 
 export { getItems, addItems, deleteItems };
