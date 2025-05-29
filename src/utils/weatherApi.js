9fd705f61ce903deb94004c7959621631e2b8923
Millie Houston <immaloser102@gmail.com>
2025-05-29 11:38:45 -0700

    Dear reviewer, you are one confusing man/woman. I don't understand how you got your job, but if code works, leave it alone. why are you making me use one function for one instance? Srsly... You make me want to quit this bootcamp

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,15 +1,13 @@
-import { handleResponse } from "./api";
+//import { handleResponse } from "./api";
+
+function handleResponse(res) { 
+  return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`); 
+}
 
 export const getWeather = ({ latitude, longitude }, APIkey) => {
   return fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
-  ).then((res) => {
-    if (handleResponse) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-  });
+  ) .then((res) => handleResponse(res));
 };
 
 export const sortWeatherData = (data) => {
