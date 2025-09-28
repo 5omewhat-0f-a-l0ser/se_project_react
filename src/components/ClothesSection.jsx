a89db504461b3c7e260c5b6a59b75ccad2d78792
Millie Houston <immaloser102@gmail.com>
2025-09-28 10:06:15 -0700

    I think I fixed the vite.config.js error of refreshing, not sure on any reviewer's end

--- src/components/ClothesSection.jsx
+++ src/components/ClothesSection.jsx
@@ -4,33 +4,37 @@ import ItemCard from "../components/ItemCard";
 
 import "../blocks/clothesselection.css";
 
-function ClothesSection({ onItemCardClick, clothingItems, onAddBtnClick, onCardLike, isLoggedIn }) {
-
-
-
-  console.log(clothingItems)
-    return (
-       <div className="clothes-selection">
-            <div className="clothes-selection__description">
-                <p className="clothes-selection__label">Your Items</p>
-                <button onClick={onAddBtnClick} className="clothes-selection__btn">+ Add New</button>
-            </div>
-            <ul className="cards__list">
-                {clothingItems
-                  .map((item) => {
-                    return (
-                      <ItemCard
-                        key={item._id}
-                        item={item}
-                        onCardClick={onItemCardClick}
-                        onCardLike={onCardLike}
-                        isLoggedIn={isLoggedIn}
-                      />
-                    );
-                })}
-            </ul>
-       </div>
-    );
+function ClothesSection({
+  onItemCardClick,
+  clothingItems,
+  onAddBtnClick,
+  onCardLike,
+  isLoggedIn,
+}) {
+  console.log(clothingItems);
+  return (
+    <div className="clothes-selection">
+      <div className="clothes-selection__description">
+        <p className="clothes-selection__label">Your Items</p>
+        <button onClick={onAddBtnClick} className="clothes-selection__btn">
+          + Add New
+        </button>
+      </div>
+      <ul className="cards__list">
+        {clothingItems.map((item) => {
+          return (
+            <ItemCard
+              key={item._id}
+              item={item}
+              onCardClick={onItemCardClick}
+              onCardLike={onCardLike}
+              isLoggedIn={isLoggedIn}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default ClothesSection;
